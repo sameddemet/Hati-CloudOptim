70ceaa84b37345b8e2ddc0a43dc8fa3ded9dd690
sameddemet <sametdemet86@gmail.com>
2023-10-02 12:35:34 +0200

    modification de webpack.config.js et npm run build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,40 @@
 const path = require('path');
-const fs = require('fs');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-
-const templateFiles = fs.readdirSync(path.resolve(__dirname, './src/'))
-  .filter((file) => path.extname(file).toLowerCase() === '.html');
-
-const htmlPluginEntries = templateFiles.map((template) => new HTMLWebpackPlugin({
-  inject: true,
-  hash: false,
-  filename: template,
-  template: path.resolve(path.resolve(__dirname, './src/'), template),
-  logo: path.resolve(path.resolve(__dirname, './src/'), 'images', 'logo.png'),
-}));
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/app.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, './dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            }
-        ]
-    },
-    plugins: [
-        ...htmlPluginEntries
-    ],
-    devServer: {
-        static: path.resolve(__dirname, './dist'),
-        port: 9000
-    }
-}
+  // Specify the mode (development or production)
+  mode: 'development', // or 'production'
+
+  // Entry point
+  entry: './src/js/app.js',
+
+  // Output configuration
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  // Module and rules for loaders
+  module: {
+    rules: [
+      // JavaScript files
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      // CSS files
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+
+  // Plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html' 
+    })
+  ]
+};
