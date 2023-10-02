1e3cef34b69319a60db9bc10b4a3106ab1dadaca
sameddemet <sametdemet86@gmail.com>
2023-10-02 11:56:29 +0200

    npm init webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const templateFiles = fs.readdirSync(path.resolve(__dirname, './src/'))
+  .filter((file) => path.extname(file).toLowerCase() === '.html');
+
+const htmlPluginEntries = templateFiles.map((template) => new HTMLWebpackPlugin({
+  inject: true,
+  hash: false,
+  filename: template,
+  template: path.resolve(path.resolve(__dirname, './src/'), template),
+  logo: path.resolve(path.resolve(__dirname, './src/'), 'images', 'logo.png'),
+}));
+
 module.exports = {
     entry: './src/js/app.js',
     output: {
