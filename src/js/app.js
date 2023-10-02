90f6eee339d46bac4f17dd5419cf8be20a473bb3
sameddemet <sametdemet86@gmail.com>
2023-10-02 11:17:55 +0200

    creation app.js et style.scss

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,29 @@
 import '../scss/style.scss';
+// Marquer un serveur comme favori
+function marquerCommeFavori(serveur) {
+    serveur.classList.toggle("favori");
+    afficherNotification("Serveur est marqué comme favori !");
+}
+
+// Afficher une notification
+function afficherNotification(message) {
+    const notification = document.createElement("div");
+    notification.classList.add("alerte");
+    notification.textContent = message;
+    document.body.appendChild(notification);
+
+    // Supprimer la notification
+    setTimeout(() => {
+        notification.remove();
+    }, 3000);
+}
+
+// Boutons "Favoris"
+const boutonsFavoris = document.querySelectorAll(".favoris-button");
+boutonsFavoris.forEach((bouton) => {
+    bouton.addEventListener("click", () => {
+        marquerCommeFavori(bouton.parentElement);
+    });
+});
+
+
